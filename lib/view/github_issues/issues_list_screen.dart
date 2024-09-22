0c04700cc6e4168a4d86b26bbac11b627c062521
shakshor <shakshor584@gmail.com>
2024-09-22 22:04:35 +0600

    responsive profile ui done

--- lib/view/github_issues/issues_list_screen.dart
+++ lib/view/github_issues/issues_list_screen.dart
@@ -27,124 +27,124 @@ class _IssuesListScreenState extends State<IssuesListScreen> {
         elevation: 0,
       ),
 
-      body: Column(
-        children:[
-          Padding(
-            padding: const EdgeInsets.symmetric(
-                vertical: 8.0,
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Search Issues',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (query) {
-                setState(() {
-                  filteredIssues = issuesData!.where((issue) => issue.title!.toLowerCase().contains(query.toLowerCase()))
-                      .toList();
-                });
-              },
-            ),
-          ),
-
-          Expanded(
-            child: FutureBuilder<List<IssueItemModel>>(
-                future: _issuesListRepo.fetchGithubIssues(),
-                builder: (context, AsyncSnapshot<List<IssueItemModel>> snapshot){
-                  // loading_indicator
-                  if(snapshot.connectionState == ConnectionState.waiting){
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  // error_showing
-                  else if(snapshot.hasError){
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  }
-                  else{
-                    if(snapshot.data!.isEmpty){
-                      return const Center(
-                        child: Text('Empty Github Issues'),
-                      );
-                    }
-                    else{
-                      log('.....inside_github_issues_list_screen.....${snapshot.data!.length}...........');
-                      issuesData = snapshot.data!;
-                      // setState(() {
-                      //                       //   issuesData = snapshot.data!;
-                      //                       // });
-
-                      // List<IssueItemModel> issuesData = snapshot.data!.where((issueItem) => issueItem.title!.contains('flutter')).toList();
-
-                      return ListView.builder(
-                          itemCount: filteredIssues.isNotEmpty ? filteredIssues.length : issuesData!.length,
-                          itemBuilder: (context, index){
-                            var issueItemData = issuesData![index];
-                            return GestureDetector(
-                              onTap: (){
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (builder) => IssueItemDetailsScreen(selectedIssue: issueItemData,)
-                                    ));
-                              },
-                              child: Card(
-                                  margin: EdgeInsets.symmetric(
-                                    vertical: 5
-                                  ),
-                                  elevation: 0,
-                                  color: Colors.blue,
-                                  child:Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children:[
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children:[
-                                            Expanded(
-                                              child: Text(
-                                                issueItemData.title.toString(),
-                                                overflow: TextOverflow.ellipsis,
-                                                maxLines: 1,
-                                              ),
-                                            ),
-                                            Text(issueItemData.createdAt.toString()),
-                                          ],
-                                        ),
-
-                                        Row(
-                                          children:[
-                                            CircleAvatar(
-                                              radius: 24,
-                                              child: Image(
-                                                image: NetworkImage(issueItemData.user!.avatarUrl!.toString()),
-                                                fit: BoxFit.contain,
-                                              ),
-                                            ),
-
-                                            Text(issueItemData.nodeId.toString()),
-                                          ],
-                                        ),
-
-                                        Divider(
-                                          thickness: 2,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                              ),
-                            );
-                          }
-                      );
-                    }
-                  }
-                }
-            ),
-          ),
-        ],
-      ),
+      // body: Column(
+      //   children:[
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(
+      //           vertical: 8.0,
+      //       ),
+      //       child: TextField(
+      //         decoration: InputDecoration(
+      //           labelText: 'Search Issues',
+      //           border: OutlineInputBorder(),
+      //         ),
+      //         onChanged: (query) {
+      //           setState(() {
+      //             filteredIssues = issuesData!.where((issue) => issue.title!.toLowerCase().contains(query.toLowerCase()))
+      //                 .toList();
+      //           });
+      //         },
+      //       ),
+      //     ),
+      //
+      //     Expanded(
+      //       child: FutureBuilder<List<IssueItemModel>>(
+      //           future: _issuesListRepo.fetchGithubIssues(),
+      //           builder: (context, AsyncSnapshot<List<IssueItemModel>> snapshot){
+      //             // loading_indicator
+      //             if(snapshot.connectionState == ConnectionState.waiting){
+      //               return const Center(
+      //                 child: CircularProgressIndicator(),
+      //               );
+      //             }
+      //             // error_showing
+      //             else if(snapshot.hasError){
+      //               return Center(
+      //                 child: Text(snapshot.error.toString()),
+      //               );
+      //             }
+      //             else{
+      //               if(snapshot.data!.isEmpty){
+      //                 return const Center(
+      //                   child: Text('Empty Github Issues'),
+      //                 );
+      //               }
+      //               else{
+      //                 log('.....inside_github_issues_list_screen.....${snapshot.data!.length}...........');
+      //                 issuesData = snapshot.data!;
+      //                 // setState(() {
+      //                 //                       //   issuesData = snapshot.data!;
+      //                 //                       // });
+      //
+      //                 // List<IssueItemModel> issuesData = snapshot.data!.where((issueItem) => issueItem.title!.contains('flutter')).toList();
+      //
+      //                 return ListView.builder(
+      //                     itemCount: filteredIssues.isNotEmpty ? filteredIssues.length : issuesData!.length,
+      //                     itemBuilder: (context, index){
+      //                       var issueItemData = issuesData![index];
+      //                       return GestureDetector(
+      //                         onTap: (){
+      //                           Navigator.push(
+      //                               context,
+      //                               MaterialPageRoute(
+      //                                   builder: (builder) => IssueItemDetailsScreen(selectedIssue: issueItemData,)
+      //                               ));
+      //                         },
+      //                         child: Card(
+      //                             margin: EdgeInsets.symmetric(
+      //                               vertical: 5
+      //                             ),
+      //                             elevation: 0,
+      //                             color: Colors.blue,
+      //                             child:Padding(
+      //                               padding: const EdgeInsets.all(8.0),
+      //                               child: Column(
+      //                                 children:[
+      //                                   Row(
+      //                                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                                     children:[
+      //                                       Expanded(
+      //                                         child: Text(
+      //                                           issueItemData.title.toString(),
+      //                                           overflow: TextOverflow.ellipsis,
+      //                                           maxLines: 1,
+      //                                         ),
+      //                                       ),
+      //                                       Text(issueItemData.createdAt.toString()),
+      //                                     ],
+      //                                   ),
+      //
+      //                                   Row(
+      //                                     children:[
+      //                                       CircleAvatar(
+      //                                         radius: 24,
+      //                                         child: Image(
+      //                                           image: NetworkImage(issueItemData.user!.avatarUrl!.toString()),
+      //                                           fit: BoxFit.contain,
+      //                                         ),
+      //                                       ),
+      //
+      //                                       Text(issueItemData.nodeId.toString()),
+      //                                     ],
+      //                                   ),
+      //
+      //                                   Divider(
+      //                                     thickness: 2,
+      //                                   ),
+      //                                 ],
+      //                               ),
+      //                             ),
+      //                         ),
+      //                       );
+      //                     }
+      //                 );
+      //               }
+      //             }
+      //           }
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- lib/view/profile/user_profile_screen.dart
+++ lib/view/profile/user_profile_screen.dart
@@ -1,73 +1,133 @@
 import 'package:flutter/material.dart';
 
-class UserProfileScreen extends StatefulWidget {
+class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({super.key});
 
   @override
-  State<UserProfileScreen> createState() => _UserProfileScreenState();
-}
-
-class _UserProfileScreenState extends State<UserProfileScreen> {
-
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff333333),
-    
+    Size size = MediaQuery.of(context).size;
+    TextScaler textFont = MediaQuery.of(context).textScaler;
+
+    return Scaffold(
+      backgroundColor: const Color(0xff333333),
+
       // preserving previous state
       body: SafeArea(
         child: Column(
           children: [
-           Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             crossAxisAlignment: CrossAxisAlignment.center,
+           Padding(
+             padding:  EdgeInsets.only(
+               top: size.width * 0.12,
+             ),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               crossAxisAlignment: CrossAxisAlignment.center,
 
-             children: [
-                CircleAvatar(
-                  radius: 120,
-                ),
-               Padding(
-                 padding: EdgeInsets.all(8.0),
-                 child: Column(
-                   children: [
-                     Text(
-                         'Francisco Miles',
-                       style: TextStyle(
-                         fontSize: 30,
-                       ),
-                     ),
-                     Text('@Francisco_miles')
-                   ],
-                 ),
-               ),
+               children: [
+                 _buildProfileImage(size),
 
-               Padding(
-                 padding: EdgeInsets.all(16.0),
-                 child: Row(
-                   mainAxisAlignment: MainAxisAlignment.center,
-                   crossAxisAlignment: CrossAxisAlignment.center,
+                 _buildProfileInfo(size, textFont),
 
-                   children: [
-                     Text('Bio: There was once..'),
-                   ],
-                 ),
-               ),
+                 _buildProfileDetails(size, textFont),
 
-               Column(
-                 mainAxisAlignment: MainAxisAlignment.start,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-
-                 children: [
-                   Text('Public Repos: 2'),
-                   Text('Public Gists: 2'),
-                   Text('Private Repos: 2'),
-                 ],
-               )
-             ],
+                 _buildGithubDetails(textFont)
+               ],
+             ),
            )
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildGithubDetails(TextScaler textFont) {
+    return Column(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+
+                 children: [
+                   Text(
+                       'Public Repos: 2',
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontSize: textFont.scale(17),
+                     ),
+                   ),
+                   Text(
+                       'Public Gists: 5',
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontSize: textFont.scale(17),
+                     ),
+                   ),
+                   Text(
+                       'Private Repos: 5',
+                     style: TextStyle(
+                       color: Colors.white,
+                       fontSize: textFont.scale(17),
+                     ),
+                   ),
+                 ],
+               );
+  }
+
+  Widget _buildProfileDetails(Size size, TextScaler textFont) {
+    return Padding(
+                 padding: EdgeInsets.symmetric(
+                   horizontal: size.width * 0.016,
+                   vertical:  size.height * 0.016,
+                 ),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   crossAxisAlignment: CrossAxisAlignment.center,
+
+                   children: [
+                     Text(
+                         'Bio: Passionate Software Engineer..',
+                       style: TextStyle(
+                         color: Colors.white,
+                         fontSize: textFont.scale(17),
+                       ),
+                     ),
+                   ],
+                 ),
+               );
+  }
+
+  Widget _buildProfileInfo(Size size, TextScaler textFont) {
+    return Padding(
+                 padding: EdgeInsets.symmetric(
+                   horizontal: size.width * 0.008,
+                   vertical:  size.height * 0.008,
+                 ),
+                 child: Column(
+                   children: [
+                     Text(
+                         'Fatin Ismam',
+                       style: TextStyle(
+                         color: Colors.white,
+                         fontSize: textFont.scale(24),
+                       ),
+                     ),
+                     Text(
+                         '@fatin_ishmam',
+                          style: TextStyle(
+                            color: const Color(0xffC9C9C9),
+                            fontSize: textFont.scale(14),
+                          ),
+                     )
+                   ],
+                 ),
+               );
+  }
+
+  Widget _buildProfileImage(Size size) {
+    return CircleAvatar(
+                  radius: size.width * 0.3,
+                  // radius: 168,
+                  backgroundImage: const AssetImage(
+                      'assets/images/person_image.png',
+                  ),
+                );
   }
 }
